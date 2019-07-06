d99321d0fac3ce139ff0b2ab631e87582fb741f9
James Coates <James@Lydias-MacBook-Pro.local>
2019-07-06 13:43:56 +0100

    correct edit event handling

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
           type="text"
           placeholder="Add todo"
           v-model="currentTodo"
+          @input="currentTodo = $event.target.value"
           @keydown.enter="addTodo()"
           @click="clearEdit()"
         />
